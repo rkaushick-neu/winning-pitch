5d737e0a96d842bb4fae08e8de776ee2fef94da2
Rishabh Kaushick <kaushick.r@northeastern.edu>
2025-10-24 22:20:09 -0400

    cleaned up code

--- backend/api/research.py
+++ backend/api/research.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-# from agents.research_agent import run_research_agent
 from agents.research_agent import research_agent
 
 router = APIRouter()
@@ -13,8 +12,6 @@ class ResearchRequest(BaseModel):
 @router.post("/generate-research")
 async def generate_research(request: ResearchRequest):
     try:
-        # TODO: Need to call the new research agent once completed
-        # markdown_report = run_research_agent(request.company_name, request.pitch_data)
         markdown_report = research_agent(request.markdown_id, request.company_name)
         return {"report": markdown_report}
     except Exception as e:
